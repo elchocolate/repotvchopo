3221b21d9c1579b5f965ae00d05a362d5bf79e1e
Fuente-TvChopo <chopofull@gmail.com>
2025-11-02 19:36:56 +0100

    yyyt

---
+++ Repositorio/plugin.video.elementum/plugin.video.elementum/resources/site-packages/platform_detect/libraries/linux_armv7_softfp/test.h
@@ -0,0 +1,14 @@
+#ifndef PLATFORM_TEST_INCLUDED
+#define PLATFORM_TEST_INCLUDED
+
+#ifdef __cplusplus
+   extern "C" {
+#endif
+
+int test ();
+
+#ifdef __cplusplus
+   }
+#endif
+
+#endif // PLATFORM_TEST_INCLUDED
